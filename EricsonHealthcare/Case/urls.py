797caf3a2254abd0ff80062e4833f8b31b033c45
Divyam Shah <divyamshah1234@gmail.com>
2024-12-13 10:39:05 +0530

    added case views

--- EricsonHealthcare/Case/urls.py
+++ EricsonHealthcare/Case/urls.py
@@ -5,7 +5,6 @@ from .views import CaseViewSet, CaseDetailsViewSet
 router = DefaultRouter()
 router.register(r'case-api', CaseViewSet, basename='case-api')
 router.register(r'case-details', CaseDetailsViewSet, basename='case-details')
-router.register(r'get_current_user_cases', CaseViewSet, basename='get_current_user_cases')
 
 urlpatterns = [
     path('', include(router.urls)),
