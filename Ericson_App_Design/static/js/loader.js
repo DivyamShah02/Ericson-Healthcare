995d2404861083a77fd8cbd184b46ef5a29a3d06
Divyam Shah <divyamshah1234@gmail.com>
2024-12-28 10:43:31 +0530

    debugged the loader

--- Ericson_App_Design/static/js/loader.js
+++ Ericson_App_Design/static/js/loader.js
@@ -1,4 +1,5 @@
 function toggle_loader() {
+    document.getElementById('offcanvasNavbar').classList.toggle('none-div');
     document.getElementById('main-content').classList.toggle('blur');
     document.getElementById('spinner').classList.toggle('none-div');
     document.getElementById('spinner').classList.toggle('spinner-container');
